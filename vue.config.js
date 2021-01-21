d18c96c89c6dccd16784ba3caab86064bb371c68
Alex Pilugin <alexpilugin@users.noreply.github.com>
2021-01-21 20:06:16 +0000

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: '/ap-naked-wines/',
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  /*
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/sass/ui.scss";`      
+      }
+    }
+  }
+  */
+}
